18ee1a48c70fee642eaa39505495d4034c01f1a9
Rory Reiff <roryreiff@Rorys-iMac.local>
2018-02-20 10:46:06 -0500

    working on contact form

--- js/main.js
+++ js/main.js
@@ -19,3 +19,37 @@ $("#hero-reviews #reviews-total").hover(
     $( this ).find(".reviews-explanation").removeClass("visible");
   }
 );
+
+//
+// Forms
+function onSubmitForm(event) {
+    grecaptcha.execute();
+    event.preventDefault();
+}
+
+function recaptchaOnSubmit(token) {
+    var generalContactForm = $('#general-contact-form');
+
+    // Submit the form via ajax
+    $.ajax({
+        url: generalContactForm.attr('action'), 
+        method: 'POST',
+        data: generalContactForm.serialize(),
+        // This causes Accepts header to be application/json
+        dataType: "json",
+        success: function() {
+          // Replace the form with a thank you message
+          generalContactForm.hide();
+          $('#thankYou').show();
+        },
+        error: function() {
+          // Replace the form with a thank you message
+          generalContactForm.hide();
+          $('#thankYou').show();
+        }
+    });
+}
+    
+$('#general-contact-form').on('submit', onSubmitForm);
+// End Forms
+//
